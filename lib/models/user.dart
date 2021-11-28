62de8ae9cd3042fc18f8d52185dc119174ff59da
SirawichDev <sirawit0676@gmail.com>
2021-11-28 17:13:51 +0700

    Z00: top player component added

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,7 +3,7 @@ class User {
   User({required this.name, required this.image});
 }
 
-List<User> topGames = [];
+List<User> topPlayer = [topU1, topU2, topU3, topU4];
 
 User u1 = User(name: "Faker", image: "assets/images/faker.jpeg");
 User u2 = User(name: "Panda", image: "assets/images/panda.jpeg");
@@ -12,3 +12,8 @@ User u5 = User(name: "long", image: "assets/images/fox.jpeg");
 User u6 = User(name: "dos", image: "assets/images/pos.png");
 User u7 = User(name: "pos", image: "assets/images/pos.jpeg");
 User u8 = User(name: "aos", image: "assets/images/ff.png");
+
+User topU1 = User(name: "Khan", image: "assets/images/top_khan.png");
+User topU2 = User(name: "Tyler1", image: "assets/images/top_tyler1.webp");
+User topU3 = User(name: "Moe", image: "assets/images/top_moe.jpeg");
+User topU4 = User(name: "Pokimane", image: "assets/images/top_pokimane.jpeg");
